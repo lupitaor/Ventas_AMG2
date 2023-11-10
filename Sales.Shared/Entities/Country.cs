b433dc4b4508bb999a748f91770fa0f0d0bc078f
Antonio Martinez Gonzalez <amartinez@itesa.edu.mx>
2023-11-09 19:04:42 -0600

    Versión02. Se creó el DataContext y la base de datos

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener un maximos de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; } = null!;
+    }
+}
